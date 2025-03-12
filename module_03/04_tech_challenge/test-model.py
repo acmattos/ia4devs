48a54fedcc17569a9400274defef033e57adc12b
acmattos <acmattos@gmail.com>
2025-03-11 21:04:22 -0300

    criando metodos para evitar repeticao de codigo

--- module_03/04_tech_challenge/test-model.py
+++ module_03/04_tech_challenge/test-model.py
@@ -1,4 +1,6 @@
 import torch
+from peft import AutoPeftModelForCausalLM
+from transformers import AutoTokenizer
 from unsloth import FastLanguageModel
 from transformers import TextStreamer
 
@@ -25,57 +27,84 @@ alpaca_prompt = """Below is an instruction that describes a task, paired with an
 ### Response:
 {}"""
 
-# 1. Primeiro carrega o modelo base
-#base_model = "unsloth/llama-3-8b-bnb-4bit"  # Modelo base do LLaMA-3
+# Carrega o modelo base
+def get_model(base_model):
+    model = AutoPeftModelForCausalLM.from_pretrained(
+        base_model, # YOUR MODEL YOU USED FOR TRAINING
+        load_in_4bit = load_in_4bit,
+    )
+    return model
+
+def get_tokenizer(base_model):
+    tokenizer = AutoTokenizer.from_pretrained(base_model)
+    return tokenizer
+
+
+def get_inputs(tokenizer, product):
+    # Tokeniza e move para GPU
+    inputs = tokenizer(
+        # Lista com os prompts
+        [  
+            # Formata o prompt usando template Alpaca
+            alpaca_prompt.format(
+                "GET THE DESCRIPTION OF THIS PRODUCT", # Instrução
+                product,                               # Produto
+                "",                                    # Resposta vazia
+            ),
+        ],
+        padding=True,
+        truncation=True,
+        return_tensors = "pt"  # Retorna tensores PyTorch 
+    ).to("cuda")  # Move para GPU
+    return inputs             
+
+def get_text_streamer(tokenizer):
+    text_streamer = TextStreamer(tokenizer, skip_prompt=True)
+    return text_streamer
+
+def get_outputs(model, inputs, text_streamer):
+    # Gera resposta
+    print("\nResposta do modelo:")
+    outputs = model.generate(
+        **inputs,
+        streamer = text_streamer,
+        max_new_tokens = 128,
+        temperature = 0.8,
+        do_sample = True
+    )
+    return outputs
+
+    # Decodifica os tokens em texto
+def decode_responses(tokenizer, outputs):
+    responses = tokenizer.batch_decode(outputs)
+    return responses
+
+def ask_the_model(base_model, product):
+    model = get_model(base_model)
+    tokenizer = get_tokenizer(base_model)
+    inputs = get_inputs(tokenizer, product)
+    text_streamer = get_text_streamer(tokenizer)
+    outputs = get_outputs(model, inputs, text_streamer)
+    decode_responses(tokenizer, outputs)
+
+def ask_questions_to_the_model(base_model):
+    model = get_model(base_model)
+    tokenizer = get_tokenizer(base_model)
+    questions = ["Girls Ballet Tutu Neon Blue",
+                 "Mog's Kittens",
+                 "The Prophet",
+                 "The Book of Revelation"]
+    for question in questions:
+        inputs = get_inputs(tokenizer, question)
+        text_streamer = get_text_streamer(tokenizer)
+        outputs = get_outputs(model, inputs, text_streamer)
+        decode_responses(tokenizer, outputs)
+
 base_model = "./lora_model_llama-3-8b-bnb-4bit"
 
-from peft import AutoPeftModelForCausalLM
-from transformers import AutoTokenizer
-model = AutoPeftModelForCausalLM.from_pretrained(
-    base_model, # YOUR MODEL YOU USED FOR TRAINING
-    load_in_4bit = load_in_4bit,
-)
-tokenizer = AutoTokenizer.from_pretrained(base_model)
+# ask_the_model(base_model, "Girls Ballet Tutu Neon Blue")
+# ask_the_model(base_model, "Mog's Kittens")
+# ask_the_model(base_model, "The Prophet")
+# ask_the_model(base_model, "The Book of Revelation")
 
-
-# model, tokenizer = FastLanguageModel.from_pretrained(
-#     model_name = base_model, # YOUR MODEL YOU USED FOR TRAINING
-#     max_seq_length = max_seq_length,
-#     dtype = dtype,
-#     load_in_4bit = load_in_4bit,
-# )
-
-# # 3. Carrega os pesos do LoRA
-# #model.load_adapter("./lora_model_llama-3-8b-bnb-4bit")
-
-# # Prepara o modelo para inferência
-# FastLanguageModel.for_inference(model)
-# Tokeniza e move para GPU
-inputs = tokenizer(
-     # Lista com os prompts
-    [  
-        # Formata o prompt usando template Alpaca
-        alpaca_prompt.format(
-            "GET THE DESCRIPTION OF THIS PRODUCT", # Instrução
-            "Girls Ballet Tutu Neon Blue",         # Produto
-            "",                                    # Resposta vazia
-        )
-    ],
-    return_tensors = "pt"  # Retorna tensores PyTorch 
-).to("cuda")               # Move para GPU
-
-# Configura streaming
-text_streamer = TextStreamer(tokenizer, skip_prompt=True)
-
-# Gera resposta
-outputs = model.generate(
-    **inputs,
-    streamer = text_streamer,
-    max_new_tokens = 128,
-    temperature = 0.9,
-    do_sample = True
-)
-# Decodifica os tokens em texto
-responses = tokenizer.batch_decode(outputs)
-print("\nResposta do modelo:")
-print(responses[0])
+ask_questions_to_the_model(base_model)
