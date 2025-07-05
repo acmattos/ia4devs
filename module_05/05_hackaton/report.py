27e42e4b91b0e7ad37b9adb13baaeb96cb4d1a8d
acmattos <acmattos@gmail.com>
2025-07-05 10:35:56 -0300

    retornando metadados do relatorio

--- module_05/05_hackaton/report.py
+++ module_05/05_hackaton/report.py
@@ -7,20 +7,21 @@ if __name__ == '__main__':
     source_file_path: str = "./data/sample/aws_01.jpg"
 
     reports_path: str = f"data/reports/{trained_dir_name}"
-    reports_json_path: str = f"{reports_path}/report.json"
-    stride_yaml  = "data/stride/stride_map.yaml"
+    #reports_json_path: str = f"{reports_path}/report.json"
+    stride_yaml_path  = "data/stride/stride_map.yaml"
     controls_yaml= "data/stride/controls_by_threat.yaml"
     conf = 0.7
 
-    results = predict(
+    results, report_json = predict(
         trained_dir_name,
         trained_model_best_path,
         source_file_path,
         conf
     )
     generate_report(
-        reports_json_path = reports_json_path,
-        stride_yaml       = stride_yaml,
+        # reports_json_path = reports_json_path,
+        reports_json_path = report_json,
+        stride_yaml_path  = stride_yaml_path,
         controls_yaml     = controls_yaml,
         conf_threshold    = 0.2,
         reports_path      = reports_path  # opcional: aumenta organização
